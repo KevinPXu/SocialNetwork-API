cb85443c857bf2628d7a5b76428a9a31359ffce9
Kevin Xu <kevinpxu21@gmail.com>
2022-11-29 01:35:26 -0600

    remove friend route

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -79,4 +79,21 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      {
+        _id: req.params.userId,
+      },
+      {
+        $pull: { friends: req.params.friendId },
+      },
+      { new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "no user with that ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
